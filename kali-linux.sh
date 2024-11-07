cc33636e576510f3b5d2e11ee95c115a4916a340
MinecraftSky <minecraftgameblocksky@gmail.com>
2024-11-07 09:46:51 +0800

    First commit

---
+++ kali-linux.sh
@@ -0,0 +1,10 @@
+DISTRO_ARCH=aarch64
+DISTRO_NAME=kali-linux
+DISTRO_COMMENT="make for MinecraftSky"
+TARBALL_STRIP_OPT=0
+TARBALL_URL['aarch64']="https://github.com/MinecraftSky/termux-proot-kail/releases/download/proot-rootfs/kali-rolling_arm64-rootfs.tar.xz"
+TARBALL_SHA256['aarch64']="c49fab64df4b427d240c0af078cca4a58ed1db6c66a321d5f3ab479007a64b1f"
+distro_setup() {
+	        run_proot_cmd touch /etc/hello-world
+	}
+				
